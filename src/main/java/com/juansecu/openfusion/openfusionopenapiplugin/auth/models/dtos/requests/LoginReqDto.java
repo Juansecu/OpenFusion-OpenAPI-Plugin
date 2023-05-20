06f3de617129cd3793b4e6ec47ee68bc42a5cc07
Juansecu <juansecoder@hotmail.com>
2023-05-20 18:25:10 -0500

    FIX - Changee Lenght annotation for Size annotation to validate user passwords

--- src/main/java/com/juansecu/openfusion/openfusionopenapiplugin/auth/models/dtos/requests/LoginReqDto.java
+++ src/main/java/com/juansecu/openfusion/openfusionopenapiplugin/auth/models/dtos/requests/LoginReqDto.java
@@ -3,22 +3,21 @@ package com.juansecu.openfusion.openfusionopenapiplugin.auth.models.dtos.request
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 public class LoginReqDto {
-    @Size(
-        max = 32,
-        message = "Invalid username length! It must be between 4 and 32 characters",
-        min = 4
-    )
     @Pattern(
         flags = {Pattern.Flag.CASE_INSENSITIVE},
         message = "Invalid username. Username can only contain alphanumeric characters, including - and _ chars",
         regexp = "^[0-9a-z_-]+$"
     )
+    @Size(
+        max = 32,
+        message = "Invalid username length! It must be between 4 and 32 characters",
+        min = 4
+    )
     private String username;
-    @Length(
+    @Size(
         max = 32,
         message = "Invalid password length! It must be between 8 and 32 characters",
         min = 8
