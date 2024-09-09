13b8fb12e4b37f96b027601d7067a5985c66a6e8
Juansecu <43616657+Juansecu@users.noreply.github.com>
2024-09-08 19:04:46 -0500

    DOCS - Retrieve OpenAPI info from pox.xml file

--- src/main/java/com/juansecu/openfusion/openfusionopenapiplugin/config/OpenApiConfig.java
+++ src/main/java/com/juansecu/openfusion/openfusionopenapiplugin/config/OpenApiConfig.java
@@ -4,25 +4,33 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    @Value("${spring.application.description}")
+    private String description;
+    @Value("${spring.application.name}")
+    private String name;
+    @Value("${spring.application.version}")
+    private String version;
+
     @Bean
     protected OpenAPI openApi() {
         final ExternalDocumentation externalDocumentation = new ExternalDocumentation()
             .description("README.md")
             .url("https://github.com/Juansecu/OpenFusion-OpenAPI-Plugin#readme");
         final Info info = new Info()
-            .description("Plugin for server applications based on OpenFusion.")
+            .description(this.description)
             .license(
                 new License()
                     .name("MIT License")
                     .url("https://github.com/Juansecu/OpenFusion-OpenAPI-Plugin/blob/main/LICENSE")
             )
-            .title("OpenFusion OpenAPI Plugin")
-            .version("1.0");
+            .title(this.name)
+            .version(this.version);
 
         return new OpenAPI()
             .externalDocs(externalDocumentation)
